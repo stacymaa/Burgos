ec588ba1d34b344e65088ed99078df88c907d3d5
stacymaa <nastasia.moskau@gmail.com>
2025-01-25 20:33:44 +0300

    add media

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,62 @@
 // Smooth Scroll
-      
+
 const links = document.querySelectorAll(".banner-btn");
 
 for (const link of links) {
-link.addEventListener("click", clickHandler);
+  link.addEventListener("click", clickHandler);
 }
 
 function clickHandler(e) {
-e.preventDefault();
-const href = this.getAttribute("href");
+  e.preventDefault();
+  const href = this.getAttribute("href");
 
-document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
-});
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
 }
 
 // Button Up
 
-window.onscroll = function(){scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 const upbuttons = document.querySelectorAll(".button-up");
 
 for (const upbutton of upbuttons) {
-    upbutton.addEventListener("click", clickHandler);
+  upbutton.addEventListener("click", clickHandler);
 }
 
 function clickHandler(e) {
-e.preventDefault();
-const href = this.getAttribute("href");
+  e.preventDefault();
+  const href = this.getAttribute("href");
 
-document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
-});
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
 }
 
-function scrollFunction(){
-    if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
-        document.getElementById('btnUp').className = 'button-up visible';
-    } else {
-        document.getElementById('btnUp').className = 'button-up hidden';
-    }
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    document.getElementById("btnUp").className = "button-up visible";
+  } else {
+    document.getElementById("btnUp").className = "button-up hidden";
+  }
+}
+
+// Menu
+
+// укажем две эти функции с помощью onclick для нужных элементов - openModal() для burger-menu и  closeModal() для modal-close
+
+// открытие модального окна
+function openModal() {
+  document.getElementById("modal").style.top = "0px";
+}
+
+// закрытие модального окна - указываем top = -400px и окно "прячется" наверх за пределами страницы
+function closeModal() {
+  document.getElementById("modal").style.top = "-400px";
 }
